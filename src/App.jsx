241ed40b2de7d0cbb28f90dbed61cd659438a37e
David Nuñez <nundavid99@gmail.com>
2020-11-13 10:38:37 -0300

    Presentational & Container Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import './App.css';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
-import {DISHES} from './shared/dishes';
+import React, {Component} from "react";
+import MainComponent from './components/MainComponent';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </div>  
-      </Navbar>
-      <Menu dishes={DISHES} />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {}
+  }
+  render(){
+    return(
+      <div>
+        <MainComponent/>
+      </div>
+    );
+  }
 }
-
 export default App;
